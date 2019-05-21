fbcfec11f5f2157abb8075b788b1902621428a1b
Kat Maldon <katharinamaldon@gmail.com>
2019-05-21 16:59:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
- 
+"SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
